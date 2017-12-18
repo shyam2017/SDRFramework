531eaea169bef6568c03e4e4436545f014204e2b
rauts <Shyam.Raut@viacomcontractor.com>
2017-12-18 15:47:15 -0500

    debug

--- com.sdrframe/src/main/java/com/sdrframe/listeners/ListernerTest.java
+++ com.sdrframe/src/main/java/com/sdrframe/listeners/ListernerTest.java
@@ -23,7 +23,7 @@ public class ListernerTest extends BaseClass implements ITestListener  {
 	public void onTestStart(ITestResult result) {
 		
 		System.out.println("Job Name of Jenkins is   --------  " +JenkinsAPI.getRunningJobName());
-		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSS"  + JenkinsAPI.getRunningBuildId());
+		System.out.println("Job ID of Jenkins is    "  + JenkinsAPI.getRunningBuildId());
 	//	String str=CommandExecutor.execCommand("ipconfig", null, null);
 	//	System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSS" +str);
 		// TODO Auto-generated method stub
